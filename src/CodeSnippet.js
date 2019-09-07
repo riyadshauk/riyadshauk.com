77840ca14afbff17b619d21c407c29208714f3b5
Riyad Shauk <riyad.shauk@gmail.com>
2019-09-06 18:43:50 -0700

    add countly tracking, update deps, resume link, logos

--- src/CodeSnippet.js
+++ src/CodeSnippet.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { rainbow } from 'react-syntax-highlighter/dist/styles/hljs';
+import { rainbow } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodeSnippet = (code: string, language: string = 'javascript') => {
   return <SyntaxHighlighter useInlineStyles="false" language={language} style={rainbow}>{code.trim()}</SyntaxHighlighter>;  
--- src/main-pages/Home.js
+++ src/main-pages/Home.js
@@ -1,6 +1,6 @@
 // @flow
-import React, { Component } from 'react';
-import styles from './Home.css';
+import React from 'react';
+import './Home.css';
 const Home = () => {
   return (
     <div>
@@ -17,13 +17,13 @@ const Home = () => {
       <p>
         Thanks for visiting!
       </p>
-      <div className={styles.photoContainer}>
-      	<img src="./our_tandem.jpg" className={styles.photo} />
-      	<p>My wife, Ayesha, and I after taking the train down to Long Beach before riding back up to Santa Monica on our (then-new) tandem bike.</p>
+      <div className="photoContainer">
+        <img src="./our_tandem.jpg" className="photo" alt="Road Tandem" />
+        <p>My wife, Ayesha, and I after taking the train down to Long Beach before riding back up to Santa Monica on our (then-new) tandem bike.</p>
       </div>
-      <div className={styles.photoContainer}>
-      	<img src="./skull_rock_climb_hike.jpg" className={styles.photo} />
-      	<p>Me trying to get out of a tough position on Skull Rock in the Pacific Palisades after hiking up there. (Photo creds: Ayesha)</p>
+      <div className="photoContainer">
+        <img src="./skull_rock_climb_hike.jpg" className="photo" alt="Hiking" />
+        <p>Me trying to get out of a tough position on Skull Rock in the Pacific Palisades after hiking up there. (Photo creds: Ayesha)</p>
       </div>
       <footer>
         <div>
@@ -35,6 +35,18 @@ const Home = () => {
         <div>
           TLS / HTTPS security: courtesy of <a href="https://www.eff.org/">Electronic Frontier Foundation</a> and their brilliant child, <a href="https://letsencrypt.org">Let's Encrypt</a>.
         </div>
+        <div>
+          Self-hosted, FOSS Analytics powered by <a href="https://github.com/Countly/countly-server">Countly</a>.
+        </div>
+        <a href="https://docs.nginx.com">
+          <img src="nginx-logo.svg" alt="Web Server Load Balancing with NGINX Plus"></img>
+        </a>
+        <a href="https://letsencrypt.org">
+          <img src="letsencrypt-logo-horizontal.svg" alt="Let's Encrypt"></img>
+        </a>
+        <a href="https://count.ly/" rel="nofollow">
+          <img className="countlyBadge" src="countly-logo.svg" alt="Countly - Product Analytics" />
+        </a>
         <div>
           Copyright © 2019, Riyad Shauk
         </div>
