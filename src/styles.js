0ab4dfe396bfc1f9ab2f7ef652cae0550a6ae694
Riyad Shauk <riyad.shauk@gmail.com>
2019-12-11 11:39:28 -0800

    resume: update style & convert to use Hooks

--- src/styles.js
+++ src/styles.js
@@ -10,6 +10,25 @@ export const AnchorContainer = styled.div`
   padding-bottom: 10px;
 `;
 
+export const FlashyAnchorContainer = styled.div`
+  a {
+    background-color: #4797D5;
+    color: #DBAB49;
+    text-decoration: none;
+    border: 1px solid #4797D5;
+    border-radius: 25px;
+    padding: 2px 5px;
+  }
+  a:hover {
+    background-color: lightcyan;
+    color: lightcoral;
+    text-decoration: none;
+    border: 1px solid lightcyan;
+  }
+  padding-top: 5px;
+  padding-bottom: 10px;
+`;
+
 export const IframeContainer = styled.div`
   background-color: white;
 `;
@@ -22,7 +41,7 @@ export const UnorderedList = styled.ul`
   display: flex;
   padding-left: 0px;
   a {
-    color: black;
+    color: #0156A6;
     text-decoration: none;
     padding: 10px;
   }
